d0045c4fb84e5fa7c9207bc2b9da1e33e6f32f09
BOM-98 <64743302+BOM-98@users.noreply.github.com>
2023-09-24 16:24:56 +0100

    Delete env.py

--- env.py
+++
@@ -1,6 +0,0 @@
-import os
-os.environ["DATABASE_URL"]="postgres://svygesgv:hqx6g9YdN--1dV06C44enqm9v0TR_EOr@tai.db.elephantsql.com/svygesgv"
-import os
-os.environ["DATABASE_URL"]="postgres://svygesgv:hqx6g9YdN--1dV06C44enqm9v0TR_EOr@tai.db.elephantsql.com/svygesgv"
-os.environ["SECRET_KEY"]="58EjeibhBu"
-os.environ["CLOUDINARY_URL"] = "cloudinary://783266428343796:WdbfvklFaYaOEMqdxunwFJHIfFg@dadlwl7fe"
