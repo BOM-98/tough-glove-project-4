401c728c2d7c7e9a9fe44f1f1064b86a33065d33
BOM-98 <64743302+BOM-98@users.noreply.github.com>
2023-09-24 15:21:08 +0000

    add gitignore

--- env.py
+++ env.py
@@ -3,4 +3,4 @@ os.environ["DATABASE_URL"]="postgres://svygesgv:hqx6g9YdN--1dV06C44enqm9v0TR_EOr
 import os
 os.environ["DATABASE_URL"]="postgres://svygesgv:hqx6g9YdN--1dV06C44enqm9v0TR_EOr@tai.db.elephantsql.com/svygesgv"
 os.environ["SECRET_KEY"]="58EjeibhBu"
-os.environ["CLOUDINARY_URL"] = "CLOUDINARY_URL=cloudinary://783266428343796:WdbfvklFaYaOEMqdxunwFJHIfFg@dadlwl7fe"
+os.environ["CLOUDINARY_URL"] = "cloudinary://783266428343796:WdbfvklFaYaOEMqdxunwFJHIfFg@dadlwl7fe"
