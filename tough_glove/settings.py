665cde978307fb00d9e094e22f1a1ce7f2429d41
BOM-98 <64743302+BOM-98@users.noreply.github.com>
2023-09-24 15:28:52 +0000

    fix: add correct allowed hosts

--- tough_glove/settings.py
+++ tough_glove/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tough-glove.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['tough-glove.herokuapp.com', 'localhost', '127.0.0.1', 'tough-glove-b997cae2826b.herokuapp.com']
 
 
 # Application definition
