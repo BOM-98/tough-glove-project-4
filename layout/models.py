a0cd945cc98c58fe5ac8cbe4d3059ed44ea260bf
BOM-98 <64743302+BOM-98@users.noreply.github.com>
2023-09-28 19:11:30 +0000

    fix: correct database models

--- layout/models.py
+++ layout/models.py
@@ -8,7 +8,7 @@ CLASSES = ((0, "Group"), (1, "Private"))
 
 class Users(models.Model):
     first_name = models.CharField(max_length=200)
-    last_name = mdoels.Charfield(max_length=200)
+    last_name = models.CharField(max_length=200)
     email = models.EmailField(max_length=200, unique=True)
     date_joined = models.DateTimeField(auto_now_add=True)
     phone_number = models.CharField(max_length=200, unique=True)
@@ -21,7 +21,7 @@ class Users(models.Model):
         return self.first_name + " " + self.last_name + " " + self.email
 
 class Classes(models.Model):
-    class_name = models.CharField()
+    class_name = models.CharField(max_length=200)
     class_description = models.TextField()
     class_type = models.IntegerField(choices=CLASSES, default=0)
     class_date = models.DateTimeField()
@@ -40,7 +40,7 @@ class Classes(models.Model):
     
 class Bookings(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    class = models.ForeignKey(Classes, on_delete=models.CASCADE)
+    class_id = models.ForeignKey(Classes, on_delete=models.CASCADE)
     booking_date = models.DateTimeField(auto_now_add=True)
     
     class Meta:
@@ -48,7 +48,7 @@ class Bookings(models.Model):
         ordering = ['-booking_date']
         
     def __str__(self):
-        return self.user + " " + self.class + " " + self.booking_date
+        return self.user + " " + self.class_id + " " + self.booking_date
 
 class BlogPosts(models.Model):
     title = models.CharField(max_length=200)
@@ -65,7 +65,21 @@ class BlogPosts(models.Model):
     
     class Meta:
         verbose_name_plural = 'BlogPosts'
-        ordering = ['-post_date']
+        ordering = ['-created_on']
         
     def __str__(self):
         return self.title + " " + self.author + " " + self.post_date
+    
+class Comments(models.Model):
+    post = models.ForeignKey(BlogPosts, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=200)
+    content = models.TextField()
+    comment_date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'Comments'
+        ordering = ['-comment_date']
+        
+    def __str__(self):
+        return self.post + " " + self.author + " " + self.comment_date
