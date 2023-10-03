b3c390e67c1be75a44cf77dfd8479c0d41e37a82
BOM-98 <64743302+BOM-98@users.noreply.github.com>
2023-10-03 10:23:21 +0000

    feat: add account register form

--- layout/forms.py
+++ layout/forms.py
@@ -6,7 +6,16 @@ from django import forms
 from .models import Classes, Bookings
 
 class CreateUserForm(UserCreationForm):
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control item'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password', 'class': 'form-control item'}))
+    
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'username', 'email']
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control item'}),
+            'first_name': forms.TextInput(attrs={'placeholder': 'First Name', 'class': 'form-control item'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Last Name', 'class': 'form-control item'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Email', 'class': 'form-control item'}),
+        }
 
