a7bb2d26eb38968dea2fe7a73e7c9ff657158b5e
BOM-98 <64743302+BOM-98@users.noreply.github.com>
2023-11-16 21:29:54 +0000

    feat: add bookingform tests to test_forms.py

--- layout/test_forms.py
+++ layout/test_forms.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from .forms import CreateUserForm, UpdateUserForm, CreateClassForm, UpdateClassForm, BookingForm
+from .models import *
 from datetime import datetime, timedelta
 
 class TestUserCreationForm(TestCase):
@@ -117,7 +118,7 @@ class TestUpdateUserForm(TestCase):
     
     # form field all fields are valid test
     def test_form_valid(self):
-            User.objects.create_user(
+            user_instance = User.objects.create_user(
             first_name = "First", 
             last_name  = "User", 
             username='newuser12',
@@ -133,12 +134,12 @@ class TestUpdateUserForm(TestCase):
                 'password1': 'complexpassword123**',
                 'password2': 'complexpassword123**'
             }
-            form = UpdateUserForm(data=form_data)
+            form = UpdateUserForm(data=form_data, instance = user_instance)
             self.assertTrue(form.is_valid())
     
     # form field missing first name test
     def test_form_invalid_missing_first_name(self):
-            User.objects.create_user(
+            user_instance =  User.objects.create_user(
             first_name = "First", 
             last_name  = "User", 
             username='newuser12',
@@ -151,12 +152,12 @@ class TestUpdateUserForm(TestCase):
                 'username': 'newuser',
                 'email': 'newuser@example2.com'
             }
-            form = UpdateUserForm(data=form_data)
+            form = UpdateUserForm(data=form_data, instance = user_instance)
             self.assertFalse(form.is_valid())
     
     # form field missing last name test
     def test_form_invalid_missing_first_name(self):
-            User.objects.create_user(
+            user_instance = User.objects.create_user(
             first_name = "First", 
             last_name  = "User", 
             username='newuser12',
@@ -169,12 +170,12 @@ class TestUpdateUserForm(TestCase):
                 'username': 'newuser',
                 'email': 'newuser@example2.com'
             }
-            form = UpdateUserForm(data=form_data)
+            form = UpdateUserForm(data=form_data, instance = user_instance)
             self.assertFalse(form.is_valid())
             
     # form field missing username test
     def test_form_invalid_missing_username(self):
-            User.objects.create_user(
+            user_instance = User.objects.create_user(
             first_name = "First", 
             last_name  = "User", 
             username='newuser12',
@@ -187,12 +188,12 @@ class TestUpdateUserForm(TestCase):
                 'last_name': 'User',
                 'email': 'newuser@example2.com',
             }
-            form = UpdateUserForm(data=form_data)
+            form = UpdateUserForm(data=form_data, instance = user_instance)
             self.assertFalse(form.is_valid())
             
     # form field missing email test
     def test_form_invalid_missing_email(self):
-        User.objects.create_user(
+        user_instance = User.objects.create_user(
             first_name = "First", 
             last_name  = "User", 
             username='newuser12',
@@ -205,7 +206,7 @@ class TestUpdateUserForm(TestCase):
                 'last_name': 'User',
                 'username': 'newuser',
             }
-        form = UpdateUserForm(data=form_data)
+        form = UpdateUserForm(data=form_data, instance = user_instance)
         self.assertFalse(form.is_valid())
         
 class TestCreateClassForm(TestCase):
@@ -323,3 +324,190 @@ class TestCreateClassForm(TestCase):
         form = CreateClassForm(data=form_data)
         self.assertFalse(form.is_valid())
         self.assertIn('slots_available', form.errors)
+        
+class TestUpdateClassForm(TestCase):
+    
+    def test_form_valid_data(self):
+        """
+        This test to ensures the UPDATECLASSFORM is valid if no fields are excluded.
+        This test creates a class instance and creates a form data dictionary without 
+        all fields included and initializes the CREATECLASSFORM with this data. 
+        It then checks:
+        1. The form is valid (self.assertTrue(form.is_valid()))
+        The inclusion of all fields should, ensure the form valid.
+        """
+        class_instance = Classes.objects.create(
+            class_name = 'Yoga',
+            class_description = 'A relaxing yoga session',
+            class_type = 0,
+            class_date = datetime.now().date(),
+            class_start_time = datetime.now().time(),
+            class_end_time = (datetime.now() + timedelta(hours=1)).time(),
+            slots_available = 10
+        )
+        
+        form_data = {
+            'class_name': 'Yoga',
+            'class_description': 'A relaxing yoga session',
+            'class_type': 0,
+            'class_date': datetime.now().date(),
+            'class_start_time': datetime.now().time(),
+            'class_end_time': (datetime.now() + timedelta(hours=1)).time(),
+            'slots_available': 10,
+        }
+
+        form = UpdateClassForm(data=form_data, instance = class_instance)
+        self.assertTrue(form.is_valid())
+        if form.is_valid():
+            instance = form.save()
+            self.assertIsNotNone(instance)
+            
+    def test_form_invalid_missing_class_type(self):
+        """
+        This test to ensures the UPDATECLASSFORM is invalid if no CLASS_TYPE field is inputed.
+        This test creates a class instance and creates a form data dictionary without 
+        the CLASS_TYPE field and initializes the CREATECLASSFORM with this data. 
+        It then checks two things:
+        1. The form is not valid (self.assertFalse(form.is_valid())).
+        2. The specific error for 'class_type' is included in the form's errors 
+        The absence of class_type should trigger a validation error, making the form invalid.
+        """
+        class_instance = Classes.objects.create(
+            class_name = 'Yoga',
+            class_description = 'A relaxing yoga session',
+            class_type = 0,
+            class_date = datetime.now().date(),
+            class_start_time = datetime.now().time(),
+            class_end_time = (datetime.now() + timedelta(hours=1)).time(),
+            slots_available = 10
+        )
+        
+        form_data = {
+            'class_name': 'Yoga',
+            'class_description': 'A relaxing yoga session',
+            'class_date': datetime.now().date(),
+            'class_start_time': datetime.now().time(),
+            'class_end_time': (datetime.now() + timedelta(hours=1)).time(),
+            'slots_available': 10,
+        }
+
+        form = UpdateClassForm(data=form_data, instance = class_instance)
+        self.assertFalse(form.is_valid())
+        self.assertIn('class_type', form.errors)
+        
+    def test_form_invalid_missing_class_date(self):
+        """
+        This test to ensures the UPDATECLASSFORM is invalid if no CLASS_DATE field is inputed.
+        This test creates a class instance and creates a form data dictionary without
+        the CLASS_DATE field and initializes the CREATECLASSFORM with this data.
+        It then checks two things:
+        1. The form is not valid (self.assertFalse(form.is_valid())).
+        2. The specific error for 'class_date' is included in the form's errors
+        The absence of class_date should trigger a validation error, making the form invalid.
+        """
+        class_instance = Classes.objects.create(
+            class_name = 'Yoga',
+            class_description = 'A relaxing yoga session',
+            class_type = 0,
+            class_date = datetime.now().date(),
+            class_start_time = datetime.now().time(),
+            class_end_time = (datetime.now() + timedelta(hours=1)).time(),
+            slots_available = 10
+        )
+        
+        form_data = {
+            'class_name': 'Yoga',
+            'class_description': 'A relaxing yoga session',
+            'class_type': 0,
+            'class_start_time': datetime.now().time(),
+            'class_end_time': (datetime.now() + timedelta(hours=1)).time(),
+            'slots_available': 10,
+        }
+        
+        form = UpdateClassForm(data=form_data, instance = class_instance)
+        self.assertFalse(form.is_valid())
+        self.assertIn('class_date', form.errors)
+    
+class TestBookingForm(TestCase):
+    
+    def test_form_valid_data(self):
+        """
+        This test to ensures the BOOKINGFORM is valid if no fields are excluded.
+        This test creates a class instance and creates a form data dictionary with
+        all fields included and initializes the BOOKINGFORM with this data.
+        It then checks:
+        1. The form is valid (self.assertTrue(form.is_valid()))
+        The inclusion of all fields should, ensure the form valid.
+        """
+        user_instance = User.objects.create_user(
+            first_name = "First", 
+            last_name  = "User", 
+            username='newuser12',
+        )
+        class_instance = Classes.objects.create(
+            class_name = 'Yoga',
+            class_description = 'A relaxing yoga session',
+            class_type = 0,
+            class_date = datetime.now().date(),
+            class_start_time = datetime.now().time(),
+            class_end_time = (datetime.now() + timedelta(hours=1)).time(),
+            slots_available = 10
+        )
+        form_data = {
+            'user': user_instance,
+            'class_id': class_instance,
+        }
+        form = BookingForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        if form.is_valid():
+            instance = form.save()
+            self.assertIsNotNone(instance)
+            
+    def test_form_invalid_missing_user(self):
+        """
+        This test to ensures the BOOKINGFORM is invalid if no USER field is inputed.
+        This test creates a class instance and creates a form data dictionary without
+        the USER field and initializes the BOOKINGFORM with this data.
+        It then checks two things:
+        1. The form is not valid (self.assertFalse(form.is_valid())).
+        2. The specific error for 'user' is included in the form's errors
+        The absence of user should trigger a validation error, making the form invalid.
+        """
+        class_instance = Classes.objects.create(
+            class_name = 'Yoga',
+            class_description = 'A relaxing yoga session',
+            class_type = 0,
+            class_date = datetime.now().date(),
+            class_start_time = datetime.now().time(),
+            class_end_time = (datetime.now() + timedelta(hours=1)).time(),
+            slots_available = 10
+        )
+        form_data = {
+            'class_id': class_instance,
+        }
+        form = BookingForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('user', form.errors)
+        
+    def test_form_invalid_missing_class_id(self):
+        """
+        This test to ensures the BOOKINGFORM is invalid if no CLASS_ID field is inputed.
+        This test creates a class instance and creates a form data dictionary without
+        the CLASS_ID field and initializes the BOOKINGFORM with this data.
+        It then checks two things:
+        1. The form is not valid (self.assertFalse(form.is_valid())).
+        2. The specific error for 'class_id' is included in the form's errors
+        The absence of class_id should trigger a validation error, making the form invalid.
+        """
+        user_instance = User.objects.create_user(
+            first_name = "First", 
+            last_name  = "User", 
+            username='newuser12',
+        )
+        form_data = {
+            'user': user_instance,
+        }
+        form = BookingForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('class_id', form.errors)
+            
