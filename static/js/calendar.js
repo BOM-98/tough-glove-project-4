46a8c2b7310335f70920adea178935000bb8422f
BOM-98 <64743302+BOM-98@users.noreply.github.com>
2023-11-23 18:02:31 +0000

    feat: remove alert popup - calendar.js

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -1,3 +1,30 @@
+/**
+ * Initializes and renders a FullCalendar instance on a webpage.
+ * 
+ * This script sets up a calendar using the FullCalendar library, tailored for displaying
+ * class events on a calendar element. It waits for the DOM content to be fully
+ * loaded before executing. The calendar is configured with various options including theme,
+ * header toolbar, initial view, event sources, and more.
+ * 
+ * Usage:
+ * - Ensure this script is included in a webpage with a DOM element having the ID 'calendar'.
+ * - The script will automatically execute upon full page load.
+ * - Requires FullCalendar library and Bootstrap 5 to be included in the webpage.
+ *
+ * Functions:
+ * - N/A
+ *
+ * Configuration:
+ * - Theme: Bootstrap 5
+ * - Header Toolbar: Includes navigation, title, and view change buttons.
+ * - Initial View: Week view with time grids.
+ * - Events Source: URL endpoint '/get_classes/'.
+ * - Event Interaction: Clicking an event shows an alert with event details.
+ * - Styling: Events are colored red.
+ * - Time Indicator: Shows the current time.
+ * - Time Slot Formatting: 24-hour format with hour and minute.
+ * - Scroll Start Time: Calendar view starts at 9 AM.
+ */
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
@@ -9,9 +36,6 @@ document.addEventListener('DOMContentLoaded', function() {
       },
       initialView: 'timeGridWeek',
       events: '/get_classes/',
-      eventClick: function(info) {
-            alert('Event: ' + info.event.title + '\nDescription: ' + info.event.start);
-      },
       eventColor: '#ff0000',
       nowIndicator: true,
       slotLabelFormat: {
